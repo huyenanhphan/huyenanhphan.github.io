6d2e5525874cf2b5d3468f75ab9d5997648ea548
huyenanhphan <hphan26@my.centennialcollege.ca>
2024-05-12 23:59:28 -0400

    update buttons

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,15 +1,21 @@
-"use client";
 
+"use client";
 import React from "react";
 import Image from "next/image";
-
-
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 
-
-
 const HeroSection = () => {
+    const handleHireMeClick = () => {
+        const contactSection = document.getElementById("contact");
+        contactSection.scrollIntoView({ behavior: "smooth" });
+    };
+
+    const handleDownloadCV = () => {
+        const cvPdfUrl = "/images/Anne Phan - Resume.pdf";
+        window.open(cvPdfUrl, "_blank");
+    };
+
     return (
         <section id="home">
             <div className="grid grid-cols-1 lg:grid-cols-12">
@@ -17,27 +23,27 @@ const HeroSection = () => {
                     <h1 className="text-4xl text-white font-extrabold mb-4 lg:text-6xl ">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-400">Hi, I am {""}</span>
                         <TypeAnimation
-      sequence={[
-        'Huyen Anh',
-        1000, 
-        'a Web Developer',
-        1000,
-        'Anne',
-        1000,
-        'an AI Student',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      repeat={Infinity}
-    />
+                            sequence={[
+                                'Huyen Anh',
+                                1000, 
+                                'a Web Developer',
+                                1000,
+                                'Anne',
+                                1000,
+                                'an AI Student',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
                     </h1>
                     <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
                         An Undergraduate Student in Software Engineering Technology - AI.
                     </p>
                     <div>
-                        <button className="px-4 py-2 rounded-full sm:w-fit w-full mr-4 mb-3 bg-gradient-to-br from-violet-500 to-pink-500 hover:from-purple-600 hover:to-pink-600">Hire Me</button>
-                        <button className="px-5 py-2 rounded-full sm:w-fit w-full bg-gradient-to-br mb-4 from-violet-500 to-pink-500  hover:from-purple-600 hover:to-pink-600">Download CV</button>
+                        <button onClick={handleHireMeClick} className="px-4 py-2 rounded-full sm:w-fit w-full mr-4 mb-3 bg-gradient-to-br from-violet-500 to-pink-500 hover:from-purple-600 hover:to-pink-600">Hire Me</button>
+                        <button onClick={handleDownloadCV} className="px-5 py-2 rounded-full sm:w-fit w-full bg-gradient-to-br mb-4 from-violet-500 to-pink-500  hover:from-purple-600 hover:to-pink-600">Download CV</button>
                     </div>
                 </motion.div>
 
@@ -52,8 +58,6 @@ const HeroSection = () => {
                         />
                     </div>
                 </motion.div>
-                
-
             </div>
         </section>      
     );
