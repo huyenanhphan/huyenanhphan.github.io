6ff7a3283039104b403cc36b6c52a4fb8885fd0d
huyenanhphan <hphan26@my.centennialcollege.ca>
2024-05-12 20:48:11 -0400

    rm comma

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -10,9 +10,9 @@ const ContactSection = () => {
         {/* <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-pink-300 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-4'></div> */}
 
         <div className='mb-3'>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Talk!</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Lets Talk!</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>I am always looking for opportunities, new friends, and more than that!
-            My inbox is always open, don't hesitate to contact me, even just a 'Hi', I sure will get back to you! ^^ </p>
+            My inbox is always open, do not hesitate to contact me, even just a "Hi", I sure will get back to you! ^^ </p>
             <div className='socials flex flex-row gap-2 '>
                 <Link href="github.com/huyenanhphan">
                 <FaGithub className='mr-2.5 ' size={25} width={700} height={700} />
